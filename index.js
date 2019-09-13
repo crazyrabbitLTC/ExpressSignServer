1e9662054e49039ee1be6896f24f1eb016f8cc60
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-09-13 19:36:13 -0400

    Finally FCKING WORKS.

--- index.js
+++ index.js
@@ -1,65 +1,22 @@
 require("dotenv").config();
-const port = process.env.PORT || 88;
+const port = process.env.PORT || 3000;
 const express = require("express");
 const EthCrypto = require("eth-crypto");
-const cors = require("cors");
 const asyncHandler = require("express-async-handler");
 const app = express();
 const { signMessage } = require("./utils/signUtils.js");
 
-//Connect to Web3
-const Web3Eth = require("web3-eth");
-const eth = new Web3Eth("ws://localhost:8545");
-
-//Not sure if we need this.
-const generatedKeyPair = EthCrypto.createIdentity();
-const trustedPrivKey =
-  process.env.TRUSTED_SIGNER_PRIVKEY || generatedKeyPair.privateKey;
-const trustedPubKey = EthCrypto.publicKeyByPrivateKey(trustedPrivKey);
-const RELAY_HUB =
-  process.env.RELAY_HUB || "0xd216153c06e857cd7f72665e0af1d7d82172f494";
-
-const RECIPIENT_ADDRESS = process.env.RECIPIENT_ADDRESS || null;
-
-//Check to be sure we have our contract addresses. Kill server if not provided.
-if (!RECIPIENT_ADDRESS) {
-  process.on("exit", function(code) {
-    return console.log(
-      "ERROR: Contract Addresses are required to run signing server. Please edit the .env"
-    );
-  });
-  process.exit(22);
-}
-
-const whitelist = ["http://localhost", "localhost:3001"];
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-};
-
-//SERVER CODE STARTS HERE:
-
-//app.use(cors(corsOptions));
 app.use(express.json());
-
-//Expose the JSON Body:
 app.post(
   "/checkSig",
   asyncHandler(async (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "localhost:3001");
+
     const obj = req.body;
+    console.log(obj);
     let result;
-
-    console.log(`Request for Signature:`);
-    console.dir(obj);
-
     try {
-      result = await signMessage(obj, trustedPrivKey);
+      result = await signMessage(obj);
     } catch (error) {
       console.log(error);
     }
@@ -75,10 +32,5 @@ app.post(
 var server = app.listen(3000, function() {
   var port = server.address().port;
   console.log("Example app listening at port %s", port);
-  console.log(
-    "TrustedPubKey is: ",
-    EthCrypto.publicKey.toAddress(trustedPubKey)
-  );
-  console.log("TrustedPrivKey is: ", trustedPrivKey);
 });
 module.exports = server;
--- utils/signUtils.js
+++ utils/signUtils.js
@@ -1,68 +1,76 @@
+const { utils } = require("@openzeppelin/gsn-provider");
+
 const EthCrypto = require("eth-crypto");
+const Web3 = require("web3");
+const {
+  utils: { toBN, soliditySha3, toChecksumAddress }
+} = require("web3");
+const web3 = new Web3("ws://localhost:8545");
 
-const signMessage = async (obj, trustedPrivKey) => {
-    const {
-      from,
-      to,
-      encodedFunctionCall,
-      txFee,
-      gasPrice,
-      gas,
-      nonce,
-      relayerAddress,
-      relayHubAddress
-    } = obj;
-    const signedMessage = await _signContractCall(
-      from,
-      to,
-      encodedFunctionCall,
-      txFee,
-      gasPrice,
-      gas,
-      nonce,
-      relayerAddress,
-      relayHubAddress,
-      trustedPrivKey
-    );
-    return { signedMessage: signedMessage };
-};
+// const signer = web3.eth.accounts.create();
+// console.log(web3.eth.accounts.wallet)
+// console.log("The signer is: ", signer);
+function fixSignature(signature) {
+  // in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
+  // signature malleability if version is 0/1
+  // see https://github.com/ethereum/go-ethereum/blob/v1.8.23/internal/ethapi/api.go#L465
+  let v = parseInt(signature.slice(130, 132), 16);
+  if (v < 27) {
+    v += 27;
+  }
+  const vHex = v.toString(16);
+  return signature.slice(0, 130) + vHex;
+}
 
-const recoverSignerAddress = (signature, message) => {
-  const signer = EthCrypto.recover(
-    signature, // signature
-    EthCrypto.hash.keccak256(message) // message hash
+const signMessage = async data => {
+  let accounts = await web3.eth.getAccounts();
+  console.log("Sign Account: ", accounts[0]);
+  return fixSignature(
+    await web3.eth.sign(
+      soliditySha3(
+        // eslint-disable-next-line max-len
+        data.relayerAddress,
+        data.from,
+        data.encodedFunctionCall,
+        toBN(data.txFee),
+        toBN(data.gasPrice),
+        toBN(data.gas),
+        toBN(data.nonce),
+        data.relayHubAddress,
+        data.to
+      ),
+      accounts[0]
+    )
   );
-  return signer;
-};
-
-const _signContractCall = async (
-  from,
-  to,
-  encodedFunctionCall,
-  txFee,
-  gasPrice,
-  gas,
-  nonce,
-  relayerAddress,
-  relayHubAddress,
-  privateKey
-) => {
-  const message = EthCrypto.hash.keccak256([
-    { type: "address", value: from },
-    { type: "address", value: to },
-    { type: "bytes", value: encodedFunctionCall },
-    { type: "uint256", value: txFee },
-    { type: "uint256", value: gasPrice },
-    { type: "uint256", value: gas },
-    { type: "uint256", value: nonce },
-    { type: "address", value: relayHubAddress },
-    { type: "address", value: relayerAddress }
-  ]);
-  const signedMessage = await EthCrypto.sign(privateKey, message);
-
-  return signedMessage;
 };
 
 module.exports = {
   signMessage
 };
+
+// const signMessage = async (
+//   from,
+//   to,
+//   encodedFunctionCall,
+//   txFee,
+//   gasPrice,
+//   gas,
+//   nonce,
+//   relayerAddress,
+//   relayHubAddress,
+//   signer
+// ) => {
+//   const hash = EthCrypto.hash.keccak256([
+//     { type: "address", value: from },
+//     { type: "address", value: to },
+//     { type: "bytes", value: encodedFunctionCall },
+//     { type: "uint256", value: txFee },
+//     { type: "uint256", value: gasPrice },
+//     { type: "uint256", value: gas },
+//     { type: "uint256", value: nonce },
+//     { type: "address", value: relayHubAddress },
+//     { type: "address", value: relayerAddress }
+//   ]);
+//   const signature = await EthCrypto.sign(signer, hash);
+//   return signature; // this takes care of removing signature malleability attacks
+// };
